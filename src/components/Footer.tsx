3636c37ea0a8b9df70663aa464c46dae4d5deec8
Mohammed Yahya <yahyamd97@gmail.com>
2025-04-02 02:27:54 +0530

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-blue-600 py-4 fixed bottom-0 left-0 w-full">
+        <footer className="bg-blue-600 py-4 bottom-0 left-0 w-full">
             <div className="flex justify-center items-center">
                 <p className="text-white text-center">
                     &copy; 2024 Star Wars App. All rights reserved.
